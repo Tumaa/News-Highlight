df88cd8423004da86a72757604c3c76946b5ae30
tumaa <ramanfatuu@gmail.com>
2019-10-12 23:49:33 +0300

    added the fuction that return the articles details

--- app/views.py
+++ app/views.py
@@ -8,8 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
 
+    general_news = get_news_sources('general')
+    business_news = get_news_sources('business')
+    entertainment_news = get_news_sources('entertainment')
+    sports_news = get_news_sources('sports')
+    technology_news = get_news_sources('technology')
+
+    title = 'Homepage'
 
 @app.route('/articles/<articles_id>')
 def articles(articles_id):
